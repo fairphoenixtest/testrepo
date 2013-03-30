898fb9827bae335a4404c934cd266a820ec3e35b
Дмитрий <@192.168.65.1>
2013-03-30 13:39:13 +0200

    коментарий

--- src/main/java/com/pb/testgit/App.java
+++ src/main/java/com/pb/testgit/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello world!" );
+        int i = 0;
+        System.out.println( "Hello world! " + (++ i) );
     }
 }
