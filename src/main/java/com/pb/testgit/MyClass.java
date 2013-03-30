43edefc5599f654a199b1d4429bf8b9bf4269d83
Madness <Madness@Ekvilibrium.Dlink>
2013-03-30 13:48:19 +0200

    ckxjvfldskjfls

---
+++ src/main/java/com/pb/testgit/MyClass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pb.testgit;
+
+/**
+ *
+ * @author Madness
+ */
+public class MyClass {
+    
+    public String word(){
+        return "Word";
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
